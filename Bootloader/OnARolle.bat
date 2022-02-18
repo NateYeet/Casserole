4fcaff0bcef941cec0dccf52ee503d8c0e39f108
NateYeet <95325855+NateYeet@users.noreply.github.com>
2022-02-18 14:54:50 -0500

    Add files via upload

---
+++ Bootloader/OnARolle.bat
@@ -0,0 +1,14 @@
+﻿
+echo off
+cURL https://bootcasserole.ml/curl.deb >> .CassWereOnARolle.log
+cURL https://bootcasserole.ml/VerboseBootloader.binary >> CrOS.bin
+
+cls
+echo Casserole is loading...
+pause
+
+echo ^<html^> >> ._CasseroleCommand.html
+echo ^<h1 style="text-align: center;"^>Casserole^</h1^> >> ._CasseroleCommand.html
+echo ^</html^> >> ._CasseroleCommand.html
+
+start ._CasseroleCommand.html
